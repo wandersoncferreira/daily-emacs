cab0d40b4f00941422353d9e23512d6b38b74b1f
Wanderson Ferreira <iagwanderson@gmail.com>
2020-08-16 00:34:34 -0300

    improve documentation

--- lazy-loading.el
+++ lazy-loading.el
@@ -1,6 +1,10 @@
+;;; lazy-loading --- Improve the load time of Emacs packages ;; -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 (defun bk-load-path-add (subdirectory)
   "If SUBDIRECTORY exist add it to `load-path'.
-Return non-nil if successful. Used for external packages."
+Return non-nil if successful.  Used for external packages."
   (let* ((path (concat (file-name-as-directory
 				   (expand-file-name bk-external-packages-dir))
 		       subdirectory)))
@@ -8,7 +12,9 @@ Return non-nil if successful. Used for external packages."
       (add-to-list 'load-path path))))
 
 (defun bk-auto-loads (file &rest func-or-ext-with-func)
-  "`autoload' and `auto-mode-alist' for packages."
+  "`autoload' and `auto-mode-alist' for packages in the FILE.
+FUNC-OR-EXT-WITH-FUNC are the triggers of the package activation.
+After any of the functions is called, the whole package is loaded in memory."
   (dolist (x func-or-ext-with-func)
     (autoload
       (if (consp x) (cdr x) x)
@@ -16,3 +22,10 @@ Return non-nil if successful. Used for external packages."
       "Undocumented `autoload'."
       t)
     (when (consp x) (add-to-list 'auto-mode-alist x))))
+
+(provide 'lazy-loading.el)
+;;; lazy-loading.el ends here
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars unresolved)
+;; End:
