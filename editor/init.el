1d342b4ee5f416ec5912254f1aaca8a39f54e8e0
Wanderson Ferreira <iagwanderson@gmail.com>
2020-09-22 23:56:26 -0300

    editor fix

--- editor/init.el
+++ editor/init.el
@@ -4,7 +4,7 @@
 
 ;; Here be dragons
 
-;; Time-stamp: <2020-09-22 23:47:52 (wand)>
+;; Time-stamp: <2020-09-22 23:56:21 (wand)>
 
 ;;; Code:
 
@@ -12,7 +12,7 @@
 ;; - https://github.com/magnars/expand-region.el
 ;; - History
 ;;   -  2020-08-16 Created
-(when (bk-load-path-add "expand-region.el")
+(when (bk/add-load-path "editor" "expand-region.el")
   (bk-auto-loads "expand-region" #'er/expand-region)
   (global-set-key (kbd "C-'") #'er/expand-region))
 
@@ -20,7 +20,7 @@
 ;; - https://github.com/magnars/change-inner.el
 ;; - History
 ;;   -  2020-08-15 Created
-(when (bk-load-path-add "change-inner.el")
+(when (bk/add-load-path "editor" "change-inner.el")
   (bk-auto-loads "change-inner" #'change-inner #'change-outer)
   (global-set-key (kbd "M-i") #'change-inner)
   (global-set-key (kbd "M-o") #'change-outer))
@@ -29,7 +29,7 @@
 ;; - https://github.com/magnars/multiple-cursors.el
 ;; - History
 ;;   -  2020-08-16 Created
-(when (bk-load-path-add "multiple-cursors.el")
+(when (bk/add-load-path "editor" "multiple-cursors.el")
   (bk-auto-loads "multiple-cursors" #'mc/mark-next-like-this #'mc/mark-previous-like-this)
   (global-set-key (kbd "C->") 'mc/mark-next-like-this)
   (global-set-key (kbd "C-<") 'mc/mark-previous-like-this))
@@ -38,7 +38,7 @@
 ;; - https://github.com/mrkkrp/fix-word
 ;; - History
 ;;   -  2020-08-17 Created
-(when (bk-load-path-add "fix-word")
+(when (bk/add-load-path "editor" "fix-word")
   (bk-auto-loads "fix-word"
                  #'fix-word-upcase
                  #'fix-word-downcase #'fix-word-capitalize)
